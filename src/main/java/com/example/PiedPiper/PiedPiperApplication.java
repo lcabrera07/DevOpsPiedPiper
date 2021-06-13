43dbece894968ffde50fb7c30b8a66cc7f9f826b
student <luis.cabrera@ngc.com>
2021-06-13 22:00:54 +0000

    project files

---
+++ src/main/java/com/example/PiedPiper/PiedPiperApplication.java
@@ -0,0 +1,13 @@
+package com.example.PiedPiper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiedPiperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiedPiperApplication.class, args);
+	}
+
+}
